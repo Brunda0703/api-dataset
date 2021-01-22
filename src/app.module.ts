2e92ca3cec090f119494d271bf56f793f35f4f75
Brunda0703 <58696156+Brunda0703@users.noreply.github.com>
2021-01-22 09:16:48 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {BooksModule} from './books/books.module';
 import { DatasetsModule } from './datasets/datasets.module';
 
 @Module({
-  imports: [DatasetsModule,
+  imports: [DatasetsModule,BooksModule,
     MongooseModule.forRoot('mongodb+srv://brunda:b070310b@cluster0-eocz5.mongodb.net/apidataset?retryWrites=true&w=majority')
   ],
   controllers: [AppController],
